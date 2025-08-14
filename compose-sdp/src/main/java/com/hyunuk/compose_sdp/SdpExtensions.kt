34c332ba201dc3be2a9350ebf205d20f70e8a3bb
hyunuk-Jeong <jhw41581329@gmail.com>
2025-08-14 09:13:23 +0900

    feat: Add ssp functions

--- compose-sdp/src/main/java/com/hyunuk/compose_sdp/SdpExtensions.kt
+++ compose-sdp/src/main/java/com/hyunuk/compose_sdp/SdpExtensions.kt
@@ -23,3 +23,25 @@ fun Int.sdp_h(): Dp = this.toFloat().sdp_h()
 
 @Composable
 fun Int.sdp_w(): Dp = this.toFloat().sdp_w()
+
+// Float 확장 함수 - 세로 비율 sp
+@Composable
+fun Float.ssp_h(): TextUnit {
+    val heightRatio = LocalHeightRatio.current
+    return (this * heightRatio).sp
+}
+
+// Float 확장 함수 - 가로 비율 sp
+@Composable
+fun Float.ssp_w(): TextUnit {
+    val widthRatio = LocalWidthRatio.current
+    return (this * widthRatio).sp
+}
+
+// Int 확장 함수 - 세로 비율 sp
+@Composable
+fun Int.ssp_h(): TextUnit = this.toFloat().ssp_h()
+
+// Int 확장 함수 - 가로 비율 sp
+@Composable
+fun Int.ssp_w(): TextUnit = this.toFloat().ssp_w()
